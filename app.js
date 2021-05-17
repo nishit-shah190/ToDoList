898ed8f740b00dc3a3d2b0d133aa58d90d5ceffb
nishit-shah190 <nishitshah190@gmail.com>
2021-05-17 15:46:21 +0545

    A separate Work List and EJS template added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 var items=[];
+let workitems=[];
 app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
@@ -18,14 +19,42 @@ app.get("/", function(req, res){
         year:"numeric"
     };
      day = today.toLocaleDateString("en-US", options);
-    res.render("list" , {kindOfDay:day , newListItems:items});
+    res.render("list" , {listTitle:day , newListItems:items});
 });
 
 app.post("/", function(req, res)
 {
-    var item = req.body.NewList;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.NewList;
+    console.log(req.body.list);
+
+    if(req.body.list === "Work")
+    {
+        workitems.push(item);
+        res.redirect("/work");
+    }
+    else
+    {
+        items.push(item);
+        res.redirect("/");
+    }
+    
+
+    
+});
+app.get("/work", function(req,res)
+{
+    res.render("list", {listTitle:"Work List", newListItems:workitems,});
+});
+
+app.post("/work" , function(req,res)
+{
+    let item = req.body.NewList;
+    workitems.push(item);
+    res.redirect("/work");
+});
+app.get("/about", function(req,res)
+{
+    res.render("about");
 })
 
 app.listen(3000 , function()
